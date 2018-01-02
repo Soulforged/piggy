62fd73952f38388ad5f68d8132cad49658e0e9aa
daniel.icardo <daniel.icardo@poincenot.com>
2018-01-02 19:20:13 -0300

    Add android back button handling.

--- src/ReduxNavigation.js
+++ src/ReduxNavigation.js
@@ -1,5 +1,6 @@
 //@flow
 import React from 'react';
+import { BackHandler } from 'react-native';
 import * as ReactNavigation from 'react-navigation';
 import { Action, ThunkAction, PromiseAction, connect } from 'react-redux';
 import AppNavigation from './AppNavigation';
@@ -9,15 +10,35 @@ type Props = {
   nav: Object
 };
 
-const ReduxNavigation = (props: Props) => {
-  const { dispatch, nav } = props;
-  const navigation = ReactNavigation.addNavigationHelpers({
-    dispatch,
-    state: nav
-  });
+class ReduxNavigation extends React.Component<Props> {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this._onBackPress.bind(this));
+  }
 
-  return (<AppNavigation navigation={navigation} />);
-};
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this._onBackPress.bind(this));
+  }
+
+  render() {
+    const { dispatch, nav } = this.props;
+    const navigation = ReactNavigation.addNavigationHelpers({
+      dispatch,
+      state: nav
+    });
+
+    return <AppNavigation navigation={navigation} />;
+  }
+
+  _onBackPress() {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0) {
+      return false;
+    }
+    dispatch(ReactNavigation.NavigationActions.back());
+    return true;
+  }
+
+}
 
 const mapStateToProps = state => ({ nav: state.nav });
 export default connect(mapStateToProps)(ReduxNavigation);
