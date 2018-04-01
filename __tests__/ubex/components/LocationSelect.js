f85b985a63828b0194f1514b1465d4637e2b1e19
Daniel Icardo <icardo.daniel@gmail.com>
2018-04-01 04:08:51 -0300

    Add prediction button press test.

--- __tests__/ubex/components/LocationSelect.js
+++ __tests__/ubex/components/LocationSelect.js
@@ -6,11 +6,20 @@ import LocationSelect from 'ubex/components/LocationSelect';
 
 const props = {
   setPositionById: jest.fn(),
-  predictions: {}
-}
+  predictions: {
+    fetching: false,
+    items: [{ id: '1', description: 'some', key: '1' }]
+  }
+};
 
 it('sets position on press', () => {
-  const wrapper = shallow(<LocationSelect ...props />);
-  wrapper.find('Button').onPress();
-  expect(setPositionById).toHaveBeenCalled(1);
+  const wrapper = shallow(<LocationSelect {...props} />);
+  const render = wrapper.dive().find('FlatList').dive().find('VirtualizedList')
+    .dive()
+    .find('CellRenderer')
+    .dive()
+    .find('Button');
+  expect(render.props().title).toBe('some');
+  render.simulate('press');
+  expect(props.setPositionById).toHaveBeenCalledTimes(1);
 });
